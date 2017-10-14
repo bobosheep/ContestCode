6a9348cf6334abf30ae8c596cb4dd3de7efb9129
bobosheep <ianpaul32@gmail.com>
2017-10-14 19:38:55 +0800

    Revert "change dic name"
    This reverts commit 7c97e1ab15db5f2273266f083a35d9a755465d07.

--- Round427(Div2)/A_KeyRace.py
+++
@@ -1,7 +0,0 @@
-s, v1, v2, t1, t2 = map(int, input().split())
-if(v1 * s + 2 * t1 < v2 * s + 2 * t2):
-    print("First")
-elif(v1 * s + 2 * t1 > v2 * s + 2 * t2):
-    print("Second")
-else:
-    print("Friendship")
